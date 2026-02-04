fbef0fa844d0bb20e3d28ecc15b485b72fb1dd58
UsmanCh1226 <usmanch@umich.edu>
2026-02-04 09:24:58 -0500

    First implementation of get_stm function

--- env/physics_engine.py
+++ env/physics_engine.py
@@ -11,3 +11,18 @@ class OrbitalPhysics:
         Returns the 6x6 State Transition Matrix (Phi) for time step dt.
         This represents the natural drift of the spacecraft.
         """
+        n = self.n
+        nt = n * dt
+        sn = np.sin(nt)
+        cn = np.cos(nt)
+        
+        # top left: position from position
+        Phi_rr = np.array([
+            4-3*cn, 0, 0],
+            [6*(sn - nt), 1, 0],
+            [0, 0, cn]
+        ])
+
+        # TODO: Implement the rest of the STM, 6x6 matrix using matrix algebra
+        return Phi_rr
+    
