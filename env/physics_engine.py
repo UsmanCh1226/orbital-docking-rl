3b7955c3e92dc647f49244460f13eedbb54139c7
UsmanCh1226 <usmanch@umich.edu>
2026-02-04 09:22:08 -0500

    Init function for orbital physics class

--- env/physics_engine.py
+++ env/physics_engine.py
@@ -1,2 +1,13 @@
 import numpy as np
 
+class OrbitalPhysics:
+    def __init__(self, orbital_radius=6771000): # Radius in meters
+    self.mu = 3.986004418e14  # Earth's gravitational parameter
+    self.a = orbital_radius    # Distance from Earth center
+    self.n = np.sqrt(self.mu / self.a**3) # Mean motion (angular velocity)
+
+    def get_stm(self, dt):
+        """
+        Returns the 6x6 State Transition Matrix (Phi) for time step dt.
+        This represents the natural drift of the spacecraft.
+        """
